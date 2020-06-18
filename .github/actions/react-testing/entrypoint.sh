9d9ca576c210894d50fb589534e17e60be06c61b
Tom Halpin <tom.halpin@dxc.com>
2020-06-18 16:05:02 +0100

    added React Testing and code coverage jobs to workflow

---
+++ .github/actions/react-testing/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#set -e
+#set -x
+set -o pipefail
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+cd "$GITHUB_WORKSPACE" || exit
+
+# Create-React-App includes ESLint and it’s own rules. Only need to create the configuration
+# via a new script command in package.json to lint react code eslint src/**/*.js --max-warnings 5 */
+
+echo "--> Running React unit tests"
+
+npm i
+
+CI=true npm test
